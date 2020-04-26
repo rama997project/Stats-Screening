7fb70aaedadc6e9909717c1abc47b6fb0754c366
GeorgHs <50588349+GeorgHs@users.noreply.github.com>
2020-04-26 18:42:37 +0200

    all changes will be revised shortly after
    all changes will be revised shortly after

--- Django_Project/PortfolioScreen/urls.py
+++ Django_Project/PortfolioScreen/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .FrontEndCode import exportingPortfolio
+#from .FrontEndCode import exportingPortfolio
 from . import views
 
 urlpatterns = [
     path('main/', views.PortfolioScreen, name='PortfolioScreen'),
     path('about/', views.about, name="about"),
-    path('ExportCsv/', exportingPortfolio.exportCSV, name='exportCSV'),
-    path('ExportPDF/', exportingPortfolio.exportPDF, name='exportPDF'),
+    #path('ExportCsv/', exportingPortfolio.exportCSV, name='exportCSV'),
+    #path('ExportPDF/', exportingPortfolio.exportPDF, name='exportPDF'),
     path('', views.table, name="portfolio-table"),
     path('table/', views.table, name="portfolio-table"),
     path('chart/', views.chart, name="portfolio-chart"),
