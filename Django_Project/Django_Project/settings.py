ffd6a5e43fae715e657f4a3dc6a95c316a470f64
GeorgHs <50588349+GeorgHs@users.noreply.github.com>
2020-05-13 18:45:19 +0200

    Update settings.py

--- Django_Project/Django_Project/settings.py
+++ Django_Project/Django_Project/settings.py
@@ -72,6 +72,9 @@ TEMPLATES = [
     },
 ]
 
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+NOSE_ARGS = ['--with-spec', '--spec-color']
+
 WSGI_APPLICATION = 'Django_Project.wsgi.application'
 
 # Database
