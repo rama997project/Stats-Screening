6c2e19c3ed141134178e63a1ac882c7ca1019c4a
GeorgHs <50588349+GeorgHs@users.noreply.github.com>
2020-05-13 19:20:09 +0200

    Update test1.py

--- Django_Project/PortfolioScreen/Tests/test1.py
+++ Django_Project/PortfolioScreen/Tests/test1.py
@@ -1,13 +1,17 @@
 
 
 import unittest
-
-from PortfolioScreen.Figure import Figure
+from .. import Stock, Figure
 
 
 class TestHelloWorld(unittest.TestCase):
-	
+
 	def test_equals(self):
 		fig = Figure
 		fig.figurename = "name1"
 		self.assertEquals(fig.figurename,"name1")
+
+	def test_secondTry(self):
+		stock = Stock
+		stock.ticker = "TKG"
+		self.assertEquals(stock.ticker,"TKG")
